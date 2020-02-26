7c1964b85d2a01b2e85966f2b621f768a343f802
epsi-rns <epsi.nurwijayadi@gmail.com>
2020-02-26 23:17:31 +0700

    Circle CI: setup github pages

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -1,8 +1,9 @@
-git config user.name "$USER_NAME"
-git config user.email "$USER_EMAIL"
+git config user.name "ci-build@example.domain"
+git config user.email "ci-build"
+touch ~/.nojekyll
 
 git checkout -b gh-pages
-git pull origin gh-pages
+# git pull origin gh-pages
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .

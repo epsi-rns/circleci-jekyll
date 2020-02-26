83e338f86bb2f4f58482cda831ae5b262ea80e78
epsi sayidina <epsi.rns@gmail.com>
2020-02-27 01:47:37 +0700

    Circle CI: Configuration

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -1,18 +1,17 @@
-# https://jtway.co/deploying-jekyll-to-github-pages-with-circleci-2-0-3eb69324bc6e
+git worktree add ../public gh-pages
+cd ../public
+git pull
 
-git config user.name "ci-build@example.domain"
-git config user.email "ci-build"
 touch ~/.nojekyll
 
 # before running anything, please make a gh-pages branch first
 # git checkout gh-pages
 
-git checkout gh-pages
-git pull origin gh-pages
+#git checkout gh-pages
+#git pull origin gh-pages
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv _site/* .
-rm -R _site/
+find . -maxdepth 1 ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv ~/source/_site/* .
 
 git add -fA
 git commit --allow-empty -m "[ci skip] $(git log master -1 --pretty=%B)"
